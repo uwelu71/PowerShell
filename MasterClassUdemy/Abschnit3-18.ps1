ae864eec680adfdf24ebf1123add623a115f1db6
uwelu71 <uwe_luethy@hotmail.com>
2023-04-27 12:11:45 +0200

    changed file

--- MasterClassUdemy/Abschnit3-18.ps1
+++ MasterClassUdemy/Abschnit3-18.ps1
@@ -7,3 +7,22 @@ Add-Content -Path $home\usernames.txt -Value 'Hans','Patrick', 'Patrick', 'Herbe
 Get-Content $home\usernames.txt
 
 (Get-Content $home\usernames.txt)[0..3]
+
+
+### Inhalt abrufen, einzigartige Namen aufrufen und in einer neue Datei speichern
+
+Get-Content -Path $home\usernames.txt |
+Sort-Object | # ist notwendig, weil Get-Unige nur zusammnehängende Daten erkennt
+Get-Unique |
+Add-Content -Path $home\usernames_unique.txt
+
+Get-Content $home\usernames_unique.txt
+
+
+###Objekte (Namen) zählen
+Get-Content -Path $home\\usernames_unique.txt | Measure-Object
+(Get-Content $home\usernames_unique.txt).Count
+
+### Vergleichen
+Compare-Object -ReferenceObject (Get-Content $home\usernames.txt) -DifferenceObject (Get-Content $home\usernames_unique.txt)
+Compare-Object -ReferenceObject (Get-Content $home\usernames.txt) -DifferenceObject (Get-Content $home\usernames_unique.txt) -PassThru
