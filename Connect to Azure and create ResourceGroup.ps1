169d86e44f8c352af69024fd49e92640ce6aa99f
uwelu71 <Uwe_Luethy@hotmail.com>
2025-12-28 13:48:38 +0100

    Initial Version

---
+++ Connect to Azure and create ResourceGroup.ps1
@@ -0,0 +1,4 @@
+$SubscriptionId = "c5365ae4-76ab-4929-907e-49d8456be106"
+
+Connect-AzAccount -Subscription $SubscriptionId
+New-AzResourceGroup -Name rg-learnbicep  -Location eastus -tag @{environment="demo"; project="Mastering Bicep"}
