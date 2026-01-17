ec6f304217b15c4129e0a12922274a44a511cabe
uwelu71 <Uwe_Luethy@hotmail.com>
2026-01-17 18:09:46 +0100

    not yet working

---
+++ New-VMSwitch.ps1
@@ -0,0 +1,18 @@
+New-VMSwitch –SwitchName “NATSwitch” –SwitchType Internal
+New-NetIPAddress –IPAddress 172.21.21.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
+New-NetNat –Name MyNATnetwork –InternalIPInterfaceAddressPrefix 172.21.21.0/24
+
+### Add-NetNatStaticMapping -NatName "VMSwitchNat" -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.2 -InternalPort 80 -ExternalPort 80
+
+
+New-VMSwitch –SwitchName “NATSwitch 192.168.200.0” –SwitchType Internal
+New-NetIPAddress –IPAddress 192.168.200.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch 192.168.200.0)"
+New-NetNat –Name MyNATnetwork –InternalIPInterfaceAddressPrefix 192.168.200.0/24
+
+New-VMSwitch –SwitchName “NATSwitch 192.168.50.0” –SwitchType Internal
+New-NetIPAddress –IPAddress 192.168.50.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch 192.168.50.0)"
+New-NetNat –Name MyNATnetwork –InternalIPInterfaceAddressPrefix 192.168.50.0/24
+
+New-VMSwitch –SwitchName “NATSwitch 192.168.100.0” –SwitchType Internal
+New-NetIPAddress –IPAddress 192.168.100.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch 192.168.100.0)"
+New-NetNat –Name MyNATnetwork –InternalIPInterfaceAddressPrefix 192.168.100.0/24
